a12702930dbf111a6c6760e8abdc8b192ae28b29
Gustavo Ferreira <gfmb94@gmail.com>
2020-03-29 20:05:21 -0300

    inserindos os incidentes que serão selecionados e visualizados

--- semanaomnistack11/aulas/backend/src/controllers/IncidentController.js
+++ semanaomnistack11/aulas/backend/src/controllers/IncidentController.js
@@ -10,7 +10,14 @@ module.exports = {
         .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page-1)*5)
-        .select('*');
+        .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'pngs.uf'
+    ]);
 
         response.header('X-Total-Count', count['count(*)']);
 
