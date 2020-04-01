6f9dfa6f2226749060a97d55665b6ef038dacc47
Gustavo Ferreira <gfmb94@gmail.com>
2020-04-01 02:04:17 -0300

    estilizando o Profile ainda estatico

--- project_react/aulas/frontend/src/pages/Profile/index.js
+++ project_react/aulas/frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {FiPower} from 'react-icons/fi';
+import {FiPower, FiTrash2} from 'react-icons/fi';
 
 import "./style.css"
 
@@ -18,6 +18,62 @@ export default function Profile(){
                     <FiPower size={18} color="#ec6708"/>
                 </button>
             </header>
+            <h1>Pedidos de Ajuda Cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CALSA:</strong>
+                    <p>Caso de procura-se um novo emprego</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Atualmente em uma empresa que não me valoriza, procuro um novo emprego para dar mais sentido a minha vida;</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 2500,00</p>
+
+                    <button>
+                        <FiTrash2 size={} color="rgb(167,137,89)"/>
+                    </button> 
+                </li>
+                <li>
+                    <strong>CALSA:</strong>
+                    <p>Caso de procura-se um novo emprego</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Atualmente em uma empresa que não me valoriza, procuro um novo emprego para dar mais sentido a minha vida;</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 2500,00</p>
+
+                    <button>
+                        <FiTrash2 size={} color="rgb(167,137,89)"/>
+                    </button> 
+                </li>
+                <li>
+                    <strong>CALSA:</strong>
+                    <p>Caso de procura-se um novo emprego</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Atualmente em uma empresa que não me valoriza, procuro um novo emprego para dar mais sentido a minha vida;</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 2500,00</p>
+
+                    <button>
+                        <FiTrash2 size={} color="rgb(167,137,89)"/>
+                    </button> 
+                </li>
+                <li>
+                    <strong>CALSA:</strong>
+                    <p>Caso de procura-se um novo emprego</p>
+
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Atualmente em uma empresa que não me valoriza, procuro um novo emprego para dar mais sentido a minha vida;</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 2500,00</p>
+
+                    <button>
+                        <FiTrash2 size={} color="rgb(167,137,89)"/>
+                    </button> 
+                </li>
+            </ul>
         </div>
     )
     
