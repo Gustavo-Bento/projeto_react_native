bc19de5af448cd4f0454731f712c1fde7445c268
Gustavo-Bento <gfmb94@gmail.com>
2020-04-02 23:03:02 -0300

    Adicionado NewIncident

---
+++ project_react/aulas/frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft} from ' react-icons/fi'
+
+import './style.css';
+
+import logoImg from '../../assets/logo.png';
+
+export default function NewIncident(){
+    return ( 
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img className="imagemlogo"src={logoImg} alt="solidariedade"/>
+
+                    <h1>Cadrastrar novo pedido ajuda</h1>
+                    <p>Descreva seu pedido detalhadamente para que a comunidade possa te ajudar.</p>
+                    <Link className="back-link" to="/register">
+                        <FiArrowLeft size={16} color="#FFEED2"/>
+                        Voltar para pagina home
+                    </Link>
+                </section>
+                <form>
+                    <input placeholder="Titulo da ajuda"/>
+                    <textarea placeholder="Descrição"/>
+                    <input placeholder="valor em reais"/>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    )
+}
--- project_react/aulas/frontend/src/pages/Register/index.js
+++ project_react/aulas/frontend/src/pages/Register/index.js
@@ -7,12 +7,11 @@ import './style.css';
 import logoImg from '../../assets/logo.png';
 
 export default function Register(){
-    return(
+    return(               
         <div className="register-container">
             <div className="content">
                 <section>
                     <img className="imagemlogo"src={logoImg} alt="solidariedade"/>
-
                     <h1>Cadastro</h1>
                     <p>Cadastre-se e entre para comunidade para ajudar e ser ajudado. Contamos com a colaboração de pessoas como você e para você</p>
                     <Link className="back-link" to="/register">
@@ -24,7 +23,7 @@ export default function Register(){
                     <input placeholder="Nome da Causa"/>
                     <input type="email" placeholder="E-mail"/>
                     <input placeholder="WhatsApp"/>
-
+   
                     <div className="input-group">
                         <input placeholder="Cidade"/>
                         <input placeholder="UF" style={{width:80}}/>
